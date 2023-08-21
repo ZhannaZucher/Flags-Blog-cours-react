f75ec4dd888afdd37469158a5c03f083fab380af
ZhannaZucher <jzucher@gmail.com>
2023-08-21 10:47:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App"></div>
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,41 @@
+import Navigation from "../components/Navigation"
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, nostrum
+        est quis voluptate porro, nesciunt, quos sed mollitia culpa corporis
+        maiores repellendus. Nam reprehenderit deleniti sunt sequi vitae
+        quibusdam voluptatem quos, non dolores corporis illum excepturi
+        distinctio delectus repellat, ratione voluptas. Voluptatibus deleniti
+        molestias quo, repellat modi eos facilis nulla iste blanditiis suscipit
+        aut accusantium ad pariatur autem sapiente distinctio quas velit
+        consequatur, sed nihil atque ut quibusdam. Totam sequi debitis libero
+        atque laudantium unde ab ipsa corporis accusamus doloribus voluptatum,
+        ducimus tempore. Pariatur eius quaerat nulla excepturi beatae, iure ipsa
+        consectetur quibusdam saepe repellendus mollitia, quia, cupiditate esse
+        delectus.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, nostrum
+        est quis voluptate porro, nesciunt, quos sed mollitia culpa corporis
+        maiores repellendus. Nam reprehenderit deleniti sunt sequi vitae
+        quibusdam voluptatem quos, non dolores corporis illum excepturi
+        distinctio delectus repellat, ratione voluptas. Voluptatibus deleniti
+        molestias quo, repellat modi eos facilis nulla iste blanditiis suscipit
+        aut accusantium ad pariatur autem sapiente distinctio quas velit
+        consequatur, sed nihil atque ut quibusdam. Totam sequi debitis libero
+        atque laudantium unde ab ipsa corporis accusamus doloribus voluptatum,
+        ducimus tempore. Pariatur eius quaerat nulla excepturi beatae, iure ipsa
+        consectetur quibusdam saepe repellendus mollitia, quia, cupiditate esse
+        delectus.
+      </p>
+    </div>
+  )
+}
+
+export default About
