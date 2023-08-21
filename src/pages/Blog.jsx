b25c27b4e9506bc4c448cd6f67f072cacb63f79b
ZhannaZucher <jzucher@gmail.com>
2023-08-21 18:36:31 +0200

    main logic

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react"
 import Logo from "../components/Logo"
 import Navigation from "../components/Navigation"
 
 const Blog = () => {
+  const [content, setContent] = useState("")
+  const [error, setError] = useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (content.length < 140) {
+      setError(true)
+    } else {
+      setError(false)
+    }
+  }
   return (
-    <div>
+    <div className="blog-container">
       <Logo />
       <Navigation />
       <h1>Blog</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="Nom" />
+        <textarea
+          style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
+          placeholder="Message"
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
+        <input type="submit" value="Envoyer" />
+      </form>
     </div>
   )
 }
